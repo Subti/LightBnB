250ab07e43cfaacc2855067ac02389263f69047b
JP Saliba <jpsaliba@outlook.com>
2023-10-19 14:51:41 -0400

    Fixed addProperty function to correctly insert contents from the form into the db.

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -19,7 +19,7 @@ const pool = new Pool({
  */
 const getUserWithEmail = function (email) {
   let queryParams = [email];
-  let queryString = `SELECT * FROM users WHERE email = $1`;
+  let queryString = `SELECT * FROM users WHERE email = $1;`;
   return pool.query(queryString, queryParams).then((result) => {
     return result.rows[0];
   });
@@ -32,7 +32,7 @@ const getUserWithEmail = function (email) {
  */
 const getUserWithId = function (id) {
   let queryParams = [id];
-  let queryString = `SELECT * FROM users WHERE id = $1`;
+  let queryString = `SELECT * FROM users WHERE id = $1;`;
   return pool.query(queryString, queryParams).then((result) => {
     return result.rows[0];
   });
@@ -45,7 +45,7 @@ const getUserWithId = function (id) {
  */
 const addUser = function (user) {
   let queryParams = [user.name, user.email, user.password];
-  let queryString = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`;
+  let queryString = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`;
   return pool.query(queryString, queryParams);
 };
 
@@ -156,10 +156,28 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+  let queryString = `INSERT INTO properties (owner_id, title, description,
+    thumbnail_photo_url, cover_photo_url, cost_per_night,
+    street, city, province, post_code,
+    country, parking_spaces,
+    number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`;
+  return pool.query(queryString, queryParams);
 };
 
 module.exports = {
